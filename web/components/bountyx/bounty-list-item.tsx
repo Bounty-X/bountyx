949198280a4519b8e6aac9493b48db5045dc6800
cipherZ <dev@cipherz.com>
2023-03-04 18:08:59 -0500

    rendering html description

--- web/components/bountyx/bounty-list-item.tsx
+++ web/components/bountyx/bounty-list-item.tsx
@@ -1,29 +1,38 @@
 import { BountyxMetadata } from '@/bountyxlib/types/bountyxdata'
+const HtmlToReactParser = require('html-to-react').Parser
 
 interface BountyListItemProps {
   bounty: BountyxMetadata
 }
 
+const htmlToReactParser = new HtmlToReactParser()
+
 export const BountyListItem = ({ bounty }: BountyListItemProps) => {
+  const reactElement = htmlToReactParser.parse(bounty.description)
+
   return (
-    <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
-      <div className="avatar">
-        <div className="w-24 rounded-full">
-          <img src={bounty.issuer.issuerLogoUrl} alt={bounty.issuer.issuerName} />
-        </div>
-        <p>{bounty.issuer.issuerName}</p>
-        {/* <p>{bounty.description}</p> */}
-        <p>{`Bounty Reward: ${bounty?.reward?.rewardAmountUsd} USD`}</p>
-      </div>
-    </div>
     // <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
-    //   <div className="w-24 rounded-full">
-    //     <img src={bounty.issuer.issuerLogoUrl} alt={bounty.issuer.issuerName} />
-    //   </div>
-    //   <div className="collapse-title text-xl font-medium">Focus me to see content</div>
-    //   <div className="collapse-content">
-    //     <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+    //   <div className="avatar">
+    //     <div className="w-24 rounded-full">
+    //       <img src={bounty.issuer.issuerLogoUrl} alt={bounty.issuer.issuerName} />
+    //     </div>
+    //     <p>{bounty.issuer.issuerName}</p>
+    //     {/* <p>{bounty.description}</p> */}
+    //     <p>{`Bounty Reward: ${bounty?.reward?.rewardAmountUsd} USD`}</p>
     //   </div>
     // </div>
+    <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+      <div className="flex flex-row">
+        <div className="w-12 rounded-full ml-2 mt-2 mr-2">
+          <img src={bounty.issuer.issuerLogoUrl} alt={bounty.issuer.issuerName} />
+        </div>
+        <div className="collapse-title text-xl font-medium">{bounty.issuer.issuerName}</div>
+      </div>
+      <div className="badge badge-outline">first</div>
+      <div className="collapse-content">
+        <div tabIndex={0} />
+        <p>{reactElement}</p>
+      </div>
+    </div>
   )
 }
