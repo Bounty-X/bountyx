63935c45e917a9e58f4da1f9f3fe61f9bbce2685
Ben Goldberger <bwgoldberger@gmail.com>
2023-03-04 16:03:25 -0700

    Update styling of certificate.

--- web/components/certificate/certificate-image-html.tsx
+++ web/components/certificate/certificate-image-html.tsx
@@ -9,7 +9,7 @@ import { HypercertMetadata } from '@/bountyxlib/types/metadata'
 
 export default function CertificateImageHtml({ metadata }: { metadata: HypercertMetadata & BountyxMetadataCollection }) {
   // Declare a new state variable, which we'll call "count"
-  const [backgroundUrl, setBackgroundUrl] = useState("/ethdenverstage.png");
+  const [backgroundUrl, setBackgroundUrl] = useState("/astronomy-bg.jpeg");
 
   const renderBounties = (): any[] => {
     if (!metadata.bounties) return []
@@ -20,17 +20,16 @@ export default function CertificateImageHtml({ metadata }: { metadata: Hypercert
     })
     return list
   }
-  
 
   return <>
-    <div className="overflow-hidden relative bg-base-200 h-[525px] w-[375px] rounded-3xl" style={{backgroundImage: 'url(/ethdenverstage.png)'}}>
+    <div className="border-2 bg-cover border--slate-700 divide-y divide--slate-700 overflow-hidden relative bg-base-200 h-[525px] w-[375px] rounded-3xl" 
+        style={{ backgroundImage: `url(${backgroundUrl})`}}>
       <div className="my-4 mx-8">
           {renderBounties()}
       </div>
-      <div className=" absolute left-0 top-[200px] h-[325px] w-[375px] bg-gradient-to-t from-slate-600 via-slate-400  to-transparent">
+      <div className=" absolute left-0 top-[300px] h-[225px] w-[375px] bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
         <div className="mx-10">
-          <div className="divider decoration-white outline-white stroke-white divide-white"></div> 
-          <div className="my-2">
+          <div className="my-4">
             <span className="text-3xl font-bold antialiased font-sans decoration-white">{metadata.name}</span>
           </div>
           <div className="my-2">
@@ -40,12 +39,12 @@ export default function CertificateImageHtml({ metadata }: { metadata: Hypercert
             <span className="text-small antialiased font-sans decoration-white">Impact: {metadata?.hypercert?.impact_scope?.value}</span>
           </div>
           <div className="my-2 flex-row">
-            <span className="text-small antialiased font-sans decoration-white">Rights:</span>
+            <span className="text-small antialiased font-sans decoration-white">Rights: public</span>
           </div>
         </div>
       </div>
     </div>
-    <div className="flex flex-row">
+    <div className="mx-4 flex flex-row">
       <div className="btn-group">
         <button className="btn btn-active">Galactic</button>
         <button className="btn">EthDenver</button>
