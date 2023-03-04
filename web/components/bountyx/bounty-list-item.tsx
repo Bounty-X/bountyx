1f82ded8cedca9e24ef4fcd36bd80df259f8e6eb
cipherZ <dev@cipherz.com>
2023-03-04 14:54:16 -0500

    fixed

--- web/components/bountyx/bounty-list-item.tsx
+++ web/components/bountyx/bounty-list-item.tsx
@@ -6,7 +6,7 @@ interface BountyListItemProps {
 
 export const BountyListItem = ({ bounty }: BountyListItemProps) => {
   return (
-    <div className="flex flex-row  rounded-box outline outline-1 outline-slate-200 mb-4">
+    <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
       <div className="avatar">
         <div className="w-24 rounded-full">
           <img src={bounty.issuer.issuerLogoUrl} alt={bounty.issuer.issuerName} />
@@ -16,5 +16,14 @@ export const BountyListItem = ({ bounty }: BountyListItemProps) => {
         <p>{`Bounty Reward: ${bounty?.reward?.rewardAmountUsd} USD`}</p>
       </div>
     </div>
+    // <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+    //   <div className="w-24 rounded-full">
+    //     <img src={bounty.issuer.issuerLogoUrl} alt={bounty.issuer.issuerName} />
+    //   </div>
+    //   <div className="collapse-title text-xl font-medium">Focus me to see content</div>
+    //   <div className="collapse-content">
+    //     <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+    //   </div>
+    // </div>
   )
 }
