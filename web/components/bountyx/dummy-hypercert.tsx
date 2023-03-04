f8ea3c98833002b4710744108408ace5265619c9
Artem B <artem.bv@pm.me>
2023-03-03 19:02:57 -0700

    form claim data + dummy hypercert

--- web/components/bountyx/dummy-hypercert.tsx
+++ web/components/bountyx/dummy-hypercert.tsx
@@ -4,17 +4,21 @@ import { getBountyxStorage } from '@/bountyxlib/bountyx-storage'
 import { BountyxMetadataCollection } from '@/bountyxlib/types/bountyxcollection'
 import { HypercertMetadata } from '@/bountyxlib/types/metadata'
 
-export const DummyHypercert = () => {
-  const [metadata, setMetadata] = useState<HypercertMetadata & BountyxMetadataCollection>({} as any)
+interface DummyHypercertProps {
+  metadata: HypercertMetadata & BountyxMetadataCollection
+}
 
-  useEffect(() => {
-    const receiveBountyxHypercert = async () => {
-      const receivedData = await getBountyxStorage().getBountyxMetadata('bafkreihqfy7tkimvuldlg33fncg5ubhkqadq33a2gd36z6sciw6f7pxbly')
-      console.log('Received bountyx data', receivedData)
-      setMetadata(receivedData)
-    }
-    receiveBountyxHypercert()
-  }, [])
+export const DummyHypercert = ({ metadata }: DummyHypercertProps) => {
+  // const [metadata, setMetadata] = useState<HypercertMetadata & BountyxMetadataCollection>({} as any)
+
+  // useEffect(() => {
+  //   const receiveBountyxHypercert = async () => {
+  //     const receivedData = await getBountyxStorage().getBountyxMetadata('bafkreihqfy7tkimvuldlg33fncg5ubhkqadq33a2gd36z6sciw6f7pxbly')
+  //     console.log('Received bountyx data', receivedData)
+  //     setMetadata(receivedData)
+  //   }
+  //   receiveBountyxHypercert()
+  // }, [])
 
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
@@ -23,17 +27,17 @@ export const DummyHypercert = () => {
       </figure>
       <div className="card-body">
         <h2 className="card-title">{metadata?.name}</h2>
-        <p>{metadata?.hypercert?.work_scope.name}</p>
-        <div className="badge-secondary badge">{metadata?.hypercert?.work_scope.display_value}</div>
-        <p>{metadata?.hypercert?.impact_scope.name}</p>
-        <div className="badge-secondary badge">{metadata?.hypercert?.impact_scope.display_value}</div>
-        <p>{metadata?.hypercert?.rights.name}</p>
-        <div className="badge-secondary badge">{metadata?.hypercert?.rights.display_value}</div>
+        <p>{metadata?.hypercert?.work_scope?.name}</p>
+        <div className="badge-secondary badge">{metadata?.hypercert?.work_scope?.display_value}</div>
+        <p>{metadata?.hypercert?.impact_scope?.name}</p>
+        <div className="badge-secondary badge">{metadata?.hypercert?.impact_scope?.display_value}</div>
+        <p>{metadata?.hypercert?.rights?.name}</p>
+        <div className="badge-secondary badge">{metadata?.hypercert?.rights?.display_value}</div>
         <div className="card-actions justify-center">
-          <p>{metadata?.hypercert?.contributors.name}</p>
-          <div className="badge-outline badge">{metadata?.hypercert?.contributors.display_value}</div>
+          <p>{metadata?.hypercert?.contributors?.name}</p>
+          <div className="badge-outline badge">{metadata?.hypercert?.contributors?.display_value}</div>
         </div>
-        <p>{metadata?.description}</p>
+        {/* <p>{metadata?.description}</p> */}
       </div>
     </div>
   )
