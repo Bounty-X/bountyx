a7734766f6dfcaf9783b80799733a9b114fad2d5
Artem B <artem.bv@pm.me>
2023-03-31 16:37:38 +0300

    feat: add canto bounties

--- web/lib/api/buidlboxApi.ts
+++ web/lib/api/buidlboxApi.ts
@@ -1,75 +1,75 @@
 import { BountyxMetadata } from '@/bountyxlib/types/bountyxdata'
 
-import { challenges } from './challenges'
+import { groupedBountyData } from './challenges'
 
-export const getBountiesForReceiver = (receiver: string): BountyxMetadata[] => {
-  if (!receiver) return []
+const getBountiesFor = (account: string, include: (account: string, receivers: string[]) => boolean): BountyxMetadata[] => {
+  const bountiesMetadata: BountyxMetadata[] = []
 
-  const bounties: BountyxMetadata[] = []
-
-  for (const challenge of challenges) {
-    for (const reward of challenge.rewards) {
-      if (receiver.toLowerCase() !== reward.receiver.receiverAddress.toLowerCase()) {
-        continue
-      }
-
-      const bountyXMetadata = {
-        name: challenge.name,
-        description: challenge.description,
+  for (const groupedBounties of groupedBountyData) {
+    for (const bounty of groupedBounties.bounties) {
+      const partialBountyXMetadata = {
+        group: groupedBounties.group.groupDisplayName,
+        name: bounty.name,
+        description: bounty.description,
         issuer: {
           issuerAddress: '0xa0facBd53826095f65CBe48F43ddba293d8FD19b',
-          issuerName: challenge.submittedByOrgName,
-          issuerLogoUrl: challenge.submittedByOrgLogo,
-          // [k: string]: unknown;
-        },
-        receiver: {
-          receiverAddress: reward.receiver.receiverAddress, // for now
-          //[k: string]: unknown;
-        },
-        reward: {
-          rewardAmountUsd: parseInt(reward.rewardAmountUsd),
-          rewardToken: challenge.rewardToken,
+          issuerName: bounty.submittedByOrgName,
+          issuerLogoUrl: bounty.submittedByOrgLogo,
           // [k: string]: unknown;
         },
         signature: '',
       }
 
-      bounties.push(bountyXMetadata)
+      if (bounty.rewardPool && bounty.rewardPoolReceivers && include(account, bounty.rewardPoolReceivers)) {
+        bountiesMetadata.push({
+          ...partialBountyXMetadata,
+          receiver: {
+            receiverAddress: account,
+          },
+          reward: {
+            rewardAmount: parseInt(bounty.rewardPool),
+            rewardToken: bounty.rewardToken,
+            rewardInToken: bounty.rewardInToken ?? false,
+          },
+        })
+      }
+
+      for (const reward of bounty.rewards) {
+        if (!include(account, [reward.receiver.receiverAddress])) {
+          continue
+        }
+
+        bountiesMetadata.push({
+          ...partialBountyXMetadata,
+          receiver: {
+            receiverAddress: account, // for now
+            //[k: string]: unknown;
+          },
+          reward: {
+            rewardAmount: parseInt(reward.rewardAmountUsd),
+            rewardToken: bounty.rewardToken,
+            rewardInToken: bounty.rewardInToken ?? false,
+            // [k: string]: unknown;
+          },
+          signature: '',
+        })
+      }
     }
   }
 
-  return bounties
+  return bountiesMetadata
+}
+
+export const getBountiesForReceiver = (receiver: string): BountyxMetadata[] => {
+  return getBountiesFor(receiver, (account: string, receivers: string[]) => {
+    return receivers.map((item) => item.toLowerCase()).indexOf(account.toLowerCase()) !== -1
+  })
 }
 
 export const getAllBounties = (): BountyxMetadata[] => {
   const bounties: BountyxMetadata[] = []
 
-  for (const challenge of challenges) {
-    for (const reward of challenge.rewards) {
-      const bountyXMetadata = {
-        name: challenge.name,
-        description: challenge.description,
-        issuer: {
-          issuerAddress: '0xa0facBd53826095f65CBe48F43ddba293d8FD19b',
-          issuerName: challenge.submittedByOrgName,
-          issuerLogoUrl: challenge.submittedByOrgLogo,
-          // [k: string]: unknown;
-        },
-        receiver: {
-          receiverAddress: reward.receiver.receiverAddress, // for now
-          //[k: string]: unknown;
-        },
-        reward: {
-          rewardAmountUsd: parseInt(reward.rewardAmountUsd),
-          rewardToken: challenge.rewardToken,
-          // [k: string]: unknown;
-        },
-        signature: '',
-      }
-
-      bounties.push(bountyXMetadata)
-    }
-  }
-
-  return bounties
+  return getBountiesFor('', (account: string, receiver: string[]) => {
+    return true
+  })
 }
