76a4655110fc97024a1252e6f2e71d7033d4ea2d
Artem B <artem.bv@pm.me>
2023-04-25 16:30:58 +0300

    feat: add banner image to landing page

--- web/app/(landing)/page.tsx
+++ web/app/(landing)/page.tsx
@@ -3,6 +3,7 @@
 import { useConnectModal } from '@rainbow-me/rainbowkit'
 // @ts-nocheck
 import Link from 'next/link'
+import Image from 'next/image'
 import { useAccount } from 'wagmi'
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <div className="hero min-h-screen">
         <div className="hero-content text-center">
           <div className="flex flex-col">
+            <Image src="/hero.png" width={1500} height={500} alt="BountyX Banner"></Image>
             <h1 className="text-5xl font-bold">Multiply Your Impact</h1>
             <h2 className="py-6 text-3xl">BountyX is a gateway between winning a bounty and the next step of your project</h2>
             <h2 className="pb-6 text-2xl">Check your eligibility to claim a Bounty Hypercert</h2>
