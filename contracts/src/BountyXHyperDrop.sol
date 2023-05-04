2606cfa60bb5605fdcabd63484a47a73722fe2c6
Artem B <artem.bv@pm.me>
2023-05-04 17:40:13 +0300

    feat: add naive root update

--- contracts/src/BountyXHyperDrop.sol
+++ contracts/src/BountyXHyperDrop.sol
@@ -2,9 +2,10 @@
 pragma solidity 0.8.19;
 
 import { IHyperDrop } from "./interfaces/IHyperDrop.sol";
-import { MerkleProof } from "openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import { MerkleProof } from "openzeppelin-contracts/utils/cryptography/MerkleProof.sol";
+import { Ownable } from "openzeppelin-contracts/access/Ownable.sol";
 
-contract BountyXHyperDrop is IHyperDrop {
+contract BountyXHyperDrop is Ownable, IHyperDrop {
 
     // @notice thrown if an address already claimed that token
     error BountyXHyperDrop__AlreadyClaimed();
@@ -12,6 +13,7 @@ contract BountyXHyperDrop is IHyperDrop {
     error BountyXHyperDrop__NotClaimable();
 
     event Claimed(address indexed to, bytes32 indexed leaf);
+    event MerkleRootUpdated(bytes32 indexed newRoot);
 
     // @notice root of the merkle tree for the hyperdrop, updateable
     bytes32 public merkleRoot;
@@ -37,7 +39,7 @@ contract BountyXHyperDrop is IHyperDrop {
     /**
      * @notice Updates the root of the merkle tree when a new drop is added to it
      */
-    function updateRoot(bytes32 newRoot) external {
-
+    function updateRoot(bytes32 newRoot) external onlyOwner {
+        merkleRoot = newRoot;
     }
 }
