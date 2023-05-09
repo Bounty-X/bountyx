881aa95c0e413d9797745fcd77edceecce794c43
Artem B <artem.bv@pm.me>
2023-05-09 16:34:47 +0300

    chore: mock winners

--- packages/bountyx-hyperdrop/src/bounties/buidlbox/winner-address-api.ts
+++ packages/bountyx-hyperdrop/src/bounties/buidlbox/winner-address-api.ts
@@ -3,5 +3,13 @@ export default function getWinnerAddress(
   bountyName: string,
   rewardAmount: number
 ) {
+  if (
+    (bountyName === "0x Labs: Build Token Swaps with 0x Swap API" &&
+      rewardAmount === 3000) ||
+    (bountyName === "Decentralized Frontend Hosting for DAOs" &&
+      rewardAmount === 5000)
+  ) {
+    return "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1190";
+  }
   return "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"; // hardhat/foundry address #9
 }
